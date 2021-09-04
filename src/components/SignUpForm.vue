5c2252a958a8d6a3b8db6b1336d4560445a0801e
Stephon Smith <smithwrestling89@gmail.com>
2021-09-04 08:52:50 -0400

    created and added form styling to signup form

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="signup">
+    <h2>Sign Up</h2>
+    <form @submit.prevent="">
+      <input
+        type="text"
+        v-model="displayName"
+        placeholder="display name"
+        required
+      />
+      <input
+        type="text"
+        v-model="email"
+        placeholder="display name"
+        required
+      />
+      <input 
+        type="text" 
+        v-model="password" 
+        placeholder="password" 
+        required 
+      />
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+    <p>Already registered? Login here!</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+
+export default {
+  name: "SignUpForm",
+  setup() {
+    const displayName = ref("");
+    const password = ref("");
+    const email = ref("")
+
+    return { displayName, password, email };
+  },
+};
+</script>
+
+<style>
+#signup {
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 50px;
+  border-radius: 10px 10px;
+  box-shadow: 5px 5px rgba(117, 119, 128, 0.5);
+  margin-top: 100px;
+  color: #22181c;
+}
+
+#signup h2 {
+  font-size: 2.5rem;
+}
+
+form {
+  max-width: 480px;
+  margin: 0 auto;
+  text-align: left;
+}
+
+input {
+  display: block;
+  margin: 10px 0;
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid #fabd80;
+  border-radius: 6px 6px;
+  box-shadow: 2px 2px rgba(117, 119, 128, 0.5);
+  outline: none;
+}
+
+#signup button {
+  padding: 10px 20px;
+  border-radius: 6px 6px;
+  box-shadow: 2px 2px rgba(117, 119, 128, 0.5);
+  background: #61e5a5;
+  color: #ffffff;
+  border: none;
+  font-weight: 700;
+}
+
+#signup button:hover {
+  background: #ffffff;
+  color: #61e5a5;
+}
+
+@media screen and (max-width: 575px) {
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/LoginForm.vue')
+  },
+  {
+    path: '/signup',
+    name: 'SignUpForm',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/SignUpForm.vue')
   }
 ]
 
