4fea9dbd75c6a647416eb23fcd5e3fef50495092
Stephon Smith <smithwrestling89@gmail.com>
2021-09-04 14:52:58 -0400

    added seeding functionality

--- src/composable/seedData.js
+++ src/composable/seedData.js
@@ -1,21 +1,48 @@
 import { LoremIpsum } from "lorem-ipsum";
-const seedData = async (num) => {
-   try {
-    const newLorem = await new LoremIpsum({
-        sentencesPerParagraph: {
-            max: 8,
-            min: 4
-        },
-        wordsPerSentence: {
-            max: 16,
-            min: 4
-        }
+import { computed, ref } from "vue";
+
+
+const seedData = (num) => {
+    const data = ref([])
+    const loopCount = ref(num)
+    const newLorem = new LoremIpsum({
+      sentencesPerParagraph: {
+          max: 8,
+          min: 4
+      },
+      wordsPerSentence: {
+          max: 16,
+          min: 4
+      }
     })
 
-    return newLorem.generateSentences(num)
-   } catch (error) {
-       return error
-   }
+    const userArray = ref(['TheMonkeyWA', 'RZayayaya', 'Hideki405', 'GodGiftedTalent', 'KnuckBuck756', 'RandomRandom', 'JustGregg', 'Smokey88', 'YouTrippinCraig'])
+
+   (async () => {
+      try {
+        for (let i = 0; i < loopCount.value; i++) {
+            const randomIndex = Math.floor(Math.random() * (userArray.value.length - 1) + 1)
+            console.log(randomIndex)
+            const randomLength = Math.floor(Math.random() * (16-4) + 4)
+            console.log(randomLength)
+            
+            const res = await fetch('http://localhost:3000/chats', {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    username: userArray.value[randomIndex],
+                    comment: newLorem.generateSentences(randomLength)
+                })         
+            })
+
+            return await res.json()
+            // const jsonRes = await res.json()
+            // data.value.push(jsonRes)   
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    })()
 }
 
-export default { seedData }
+export default seedData
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,50 +1,70 @@
 <template>
-
-<button @click="seedData(4)">Seed Data</button>
+  <div v-for="user in loremLoop" :key="user.id">
+    <p>{{ user.username }}</p>
+    {{ user.comment}}
+  </div>
+  <button @click="loremLoop">Seed Data</button>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
-import { LoremIpsum } from "lorem-ipsum";
-import { watchEffect } from '@vue/runtime-core';
+import { computed, ref } from "@vue/reactivity";
+import { LoremIpsum } from 'lorem-ipsum'
 
 export default {
   name: "Chatroom",
   setup() {
-  const data = ref('')
-  const newLorem = new LoremIpsum({
-    sentencesPerParagraph: {
+    const data = ref([])
+    const uri = ref('http://localhost:3000/chats')
+    const newLorem = new LoremIpsum({
+      sentencesPerParagraph: {
         max: 8,
-        min: 4
-    },
-    wordsPerSentence: {
+        min: 4,
+      },
+      wordsPerSentence: {
         max: 16,
-        min: 4
-    }
-  })
+        min: 4,
+      },
+    });
 
-  const userArray = ['TheMonkeyWA', 'RZayayaya', 'Hideki405', 'GodGiftedTalent', 'KnuckBuck756', 'RandomRandom', 'JustGregg', 'Smokey88', 'YouTrippinCraig']
+    const userArray = ref([
+      "TheMonkeyWA",
+      "RZayayaya",
+      "Hideki405",
+      "GodGiftedTalent",
+      "KnuckBuck756",
+      "RandomRandom",
+      "JustGregg",
+      "Smokey88",
+      "YouTrippinCraig",
+    ]);
 
-    return { data }
-  }
-  // data() {
-  //   return {
-  //     seedData: (num) => {
-  //       const newLorem = new LoremIpsum({
-  //         sentencesPerParagraph: {
-  //           max: 8,
-  //           min: 4,
-  //         },
-  //         wordsPerSentence: {
-  //           max: 16,
-  //           min: 4,
-  //         },
-  //       });
+    const loremLoop = computed(async () => {
+      try {
+        
+        // for (let i = 0; i < 100; i++) {
+        //   const randomIndex = Math.floor(
+        //     Math.random() * (userArray.value.length - 1) + 1
+        //   );
+        //   const randomLength = Math.floor(Math.random() * (16 - 4) + 4);
+        //   const res = await fetch(uri.value, {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/json" },
+        //     body: JSON.stringify({
+        //       username: userArray.value[randomIndex],
+        //       comment: newLorem.generateSentences(randomLength),
+        //     }),
+        //   })
 
-  //       return console.log(newLorem.generateSentences(num));
-  //     },
-  //   };
-  // },
+        //   data.value = await res.json()
+
+        // }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    return {loremLoop, userArray, newLorem}
+  },
 };
 </script>
 
