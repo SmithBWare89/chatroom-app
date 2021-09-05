e571b0e6524ec87cb51863b42f564d8c3eb4ddc4
Stephon Smith <smithwrestling89@gmail.com>
2021-09-05 01:19:15 -0400

    set up initial view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <NavBar />
+    <!-- <NavBar /> -->
     <router-view />
   </div>
 </template>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,23 @@
 <template>
-  <div id="chat">
-    <h1>Hi</h1>
+  <div v-if="login">
+    <LoginForm />
+  </div>
+  <div v-else>
+    <SignUpForm />
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import LoginForm from '../components/LoginForm.vue'
+import SignUpForm from '../components/SignUpForm.vue'
 export default {
   name: 'Welcome',
+  components: { LoginForm, SignUpForm },
   setup() {
+    const login = ref(true)
 
+    return { login }
   }
 }
 </script>
